e77cb129f114dd3e2da6d0aec42889fbc272afff
NhemNhem <155875453+NhomNhem@users.noreply.github.com>
2025-05-04 17:41:00 +0700

    update

--- assets/js/template.js
+++ assets/js/template.js
@@ -4,3 +4,44 @@
 if ('baguetteBox' in window && document.querySelectorAll('[data-bss-baguettebox]').length > 0) {
    baguetteBox.run('[data-bss-baguettebox]', { animation: 'slideIn' });
 }
+
+// Gắn sự kiện vào button có ID 'roadmapButton'
+const roadmapButton = document.getElementById('roadmapButton');
+if (roadmapButton) {
+    roadmapButton.addEventListener('click', () => {
+        window.location.href = 'roadmap.html'; // Chuyển hướng đến roadmap.html
+    });
+}
+
+// Gắn sự kiện vào button có ID 'xylosButton'
+const xylosButton = document.getElementById('xylosButton');
+if (xylosButton) {
+    xylosButton.addEventListener('click', () => {
+        window.location.href = 'articles/xylos.html'; // Chuyển hướng đến xylos.html
+    });
+}
+
+// Gắn sự kiện vào button có ID 'originButton'
+const originButton = document.getElementById('originButton');
+if (originButton) {
+    originButton.addEventListener('click', () => {
+        window.location.href = 'articles/origin.html'; // Chuyển hướng đến origin.html
+    });
+}
+
+
+// Gắn sự kiện vào button có ID 'uranastraButton'
+const uranastraButton = document.getElementById('uranastraButton');
+if (uranastraButton) {
+    uranastraButton.addEventListener('click', () => {
+        window.location.href = 'articles/uranastra.html'; // Chuyển hướng đến uranastra.html
+    });
+}
+
+// Gắn sự kiện vào button có ID 'letoButton'
+const letoButton = document.getElementById('letoButton');
+if (letoButton) {
+    letoButton.addEventListener('click', () => {
+        window.location.href = 'articles/leto.html'; // Chuyển hướng đến leto.html
+    });
+}
